9fe29a8f7bb8b540e8abddb163a70f6ec0ac4858
ramsey.bland <ramsey.bland@gmail.com>
2021-05-04 21:47:02 -0600

    fixed navigation

---
+++ course-tracker/course-tracker/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using course_tracker.Models;
+using course_tracker.ViewModels;
+using Xamarin.Forms;
+
+namespace coursetracker.ViewModels
+{
+    public class CoursesViewModel : BaseViewModel
+    {
+        public ObservableCollection<Course> Courses { get; set; }
+        public Command LoadCoursesCommand { get; set; }
+
+        private readonly Term _term;
+
+        public CoursesViewModel(Term term = null)
+        {
+            if (term == null) term = GetDefaultTerm();
+            _term = term;
+
+            if (term != null)
+            {
+                Title = $"{term.Title}'s courses";
+            }
+            else
+            {
+                Title = "No Term Selected";
+            }
+            Courses = new ObservableCollection<Course>();
+            LoadCoursesCommand = new Command(async () => await LoadCourses());
+
+            //MessagingCenter.Subscribe<NewTermPage, Term>(this, "AddTerm", async (obj, term) =>
+            //{
+            //    await LoadTerms();
+            //});
+        }
+
+        async Task LoadCourses()
+        {
+            IsBusy = true;
+            try
+            {
+                Courses.Clear();
+                if (_term == null) return;
+                var courses = await SqliteConn.Table<Course>().Where(c => c.TermId == _term.Id).OrderBy(term => term.Start).ToListAsync();
+                foreach (var course in courses)
+                {
+                    Courses.Add(course);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
---
+++ course-tracker/course-tracker/Views/CoursesPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using course_tracker.Models;
+using coursetracker.ViewModels;
+using Xamarin.Forms;
+
+namespace course_tracker.Views
+{
+    public partial class CoursesPage : ContentPage
+    {
+        CoursesViewModel viewModel;
+
+        public CoursesPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new CoursesViewModel();
+        }
+
+        public CoursesPage(Term term)
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new CoursesViewModel(term);
+        }
+
+        async void OnCourseSelected(object sender, EventArgs args)
+        {
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Courses.Count == 0) viewModel.IsBusy = true;
+        }
+    }
+}
